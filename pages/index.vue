9a050e52a6f1250f0a428d43d5d726371210a6b7
JoeRourke123 <josephrourke1@gmail.com>
2020-10-11 14:35:17 +0100

    Revamped the site, models a command line interface - has some basic commands

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="terminal is-dark">
     <div class="flex-container">
-      <div class="log-list"></div>
-      <div class="input-area">
-        <span>></span>
-        <input type="text">
+      <div class="log-list">
+        <div class="command" v-for="(line, index) in log">
+          <span class="line" v-if="lines === index">
+            <span class="is-yellow">joe-rourke</span>@<span class="is-red">portfolio</span> > <span @keydown="commChanged" id="editor" :contenteditable="lines === index"></span><span id="caret"></span>
+          </span>
+          <span v-else>
+            <span class="is-yellow">joe-rourke</span>@<span class="is-red">portfolio</span> > {{ line }}
+          </span>
+          <div class="results" v-if="index < lines">
+            <br />
+            <span v-html="results[index]"></span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -14,6 +23,102 @@
 
 export default {
   name: 'Terminal',
+  data() {
+    return {
+      command: "",
+      lines: 0,
+      log: [""],
+
+      results: [],
+
+      caretOn: false,
+
+      viewingIndex: 0,
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      if(document.getElementById("caret") != null) {
+        if(document.getElementById("caret").innerText === "▋") {
+          document.getElementById("caret").innerText = "";
+        } else {
+          document.getElementById("caret").innerText = "▋";
+        }
+      }
+    }, 500);
+
+    this.focusInput();
+  },
+  methods: {
+    commChanged(e) {
+      console.log(e);
+      try {
+        if(e.which === 13) {
+          e.preventDefault();
+
+          this.log[this.lines] = e.target.innerText;
+
+          this.lines++;
+          this.viewingIndex = this.lines;
+
+          this.runCommand(this.lines - 1, this.log[this.lines - 1]);
+
+          this.log.push("");
+
+          setTimeout(() => {
+            this.focusInput();
+          }, 75);
+        } else if(e.which === 38 && this.viewingIndex > 0) {
+          e.target.innerText = this.log[--this.viewingIndex];
+        } else if(e.which === 40) {
+          if(this.viewingIndex < this.lines) {
+            e.target.innerText = this.log[++this.viewingIndex];
+          }
+        } else if(e.which === 37 || e.which === 39) {
+          e.preventDefault();
+        }
+      } catch(e) {
+        console.log(e);
+      }
+    },
+    focusInput() {
+      if(document.getElementById("editor")) {
+        document.getElementById("editor").focus();
+      }
+    },
+    runCommand(index, command) {
+      let parsedArgs = command.toLowerCase().split(" ");
+      let commOne = parsedArgs[0];
+
+      switch(commOne) {
+        case "hello":
+          this.results[index] = "Hello there!";
+          break;
+        case "clear":
+          break;
+        case "github":
+        case "linkedin":
+          let url = this.$store.state["urls"][commOne];
+          this.results[index] = "";
+          window.open(url, "_blank");
+          break;
+        case "help":
+          let helpData = this.$store.state[commOne];
+          this.results[index] = "";
+
+          for(const key in helpData) {
+            this.results[index] += `${key}</br>`;
+            for(const comm in helpData[key]) {
+              this.results[index] += `&emsp;&emsp;${comm}&emsp;&emsp;${ helpData[key][comm] }</br>`
+            }
+          }
+
+          break;
+        default:
+          this.results[index] = `${commOne} is not a recognised command.<br /> Run 'help' to see the available commands.`
+      }
+    }
+  },
 }
 </script>
 
@@ -22,10 +127,15 @@ export default {
     width: 100vw;
     height: 100vh;
     margin: 0;
-    padding: 0;
+    padding: 20px;
     overflow-y: scroll;
     overflow-x: hidden;
+
+    caret-color: transparent;
+
+    font-family: "Courier", monospace;
     background-color: #222222;
+    color: #F5F5F5;
 
     -ms-overflow-style: none;  /* IE and Edge */
     scrollbar-width: none;  /* Firefox */
@@ -35,62 +145,8 @@ export default {
     display: none;
   }
 
-  .flex-container {
-    display: flex;
-    flex-flow: column;
-    height: 100%;
-  }
-
-  .flex-container .log-list {
-    overflow-y: scroll;
-    flex-grow: 2;
-    height: calc(100vh - 50px);
-    width: 100%;
-
-    -ms-overflow-style: none;  /* IE and Edge */
-    scrollbar-width: none;  /* Firefox */
-  } .log-list::-webkit-scrollbar {
-         display: none;
-  }
-
-  .flex-container .input-area {
-    width: 100%;
-    flex-grow: 0;
-    height: 50px;
-    display: flex;
-    flex-flow: row;
-    align-items: flex-end;
-    justify-items: center;
-
-    font-family: "Courier", monospace;
-    font-size: 1.2em;
-    color: mediumseagreen;
-  }
-
-  .flex-container .input-area span {
-    height: 100%;
-    padding-top: 12px;
-    padding-left: 12px;
-    font-weight: bold;
-  }
-
-  .flex-container .input-area input {
-    height: 100%;
-    width: 100%;
-    padding-left: 20px;
-    padding-right: 40px;
-
-    background: transparent;
-    border: none;
-
-    caret-color: transparent;
-
-    font-family: "Courier", monospace;
-    font-size: 1.2em;
-    color: mediumseagreen;
-  }
-  .flex-container .input-area input:focus{
-    outline: none;
+  [contenteditable]:focus {
+    outline: 0 solid transparent;
   }
 
   body, html {
@@ -99,5 +155,13 @@ export default {
     padding: 0 !important;
     width: 100% !important;
     height: 100vh !important;
+  }
+
+  .is-yellow {
+    color: orange;
+  }
+
+  .is-red {
+    color: indianred;
   }
 </style>
---
+++ store/index.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+  help: {
+    "seeing my work": {
+      "ls": "lists all my previous and current projects",
+      "github": "opens my GitHub page",
+      "linkedin": "opens my LinkedIn page"
+    }
+  },
+  hello: {
+    "there": "General Kenobi."
+  },
+  urls: {
+    "github": "https://github.com/joerourke123",
+    "linkedin": "https://linkedin.com/in/joe-rourke",
+  }
+});
