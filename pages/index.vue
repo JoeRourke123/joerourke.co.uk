0050fb4c61d251705f90e9c7b30945bc80bd6dba
JoeRourke123 <josephrourke1@gmail.com>
2020-10-14 18:07:25 +0100

    Initial commit of project files

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,23 @@
     <div class="flex-container">
       <div class="log-list">
         <div class="command" v-for="(line, index) in log">
-          <span class="line" v-if="lines === index">
-            <span class="is-yellow">joe-rourke</span>@<span class="is-red">portfolio</span> > <span @keydown="commChanged" id="editor" :contenteditable="lines === index"></span><span id="caret"></span>
+          <span class="line" v-if="lines === index && readyForNextLine">
+            <span class="is-yellow">joe-rourke</span>@<span class="is-red">portfolio</span> $ <span
+            @keydown="commChanged" id="editor" :contenteditable="lines === index"></span><span id="caret"></span>
           </span>
           <span v-else>
-            <span class="is-yellow">joe-rourke</span>@<span class="is-red">portfolio</span> > {{ line }}
+            <span class="is-yellow">joe-rourke</span>@<span class="is-red">portfolio</span> $ {{ line }}
           </span>
-          <div class="results" v-if="index < lines">
-            <br />
+          <div class="results" v-if="index < lines || (index === lines && !readyForNextLine)">
+            <br/>
             <span v-html="results[index]"></span>
           </div>
+        </div>
+        <div class="form" v-if="!readyForNextLine && form.active">
+          <span>{{ form.promptText }}</span> <span id="editor" @keydown="commChanged" contenteditable></span><span id="caret"></span>
+        </div>
+        <div class="hangman" v-else-if="!readyForNextLine && hangman.active">
+          <span>Your Guess: </span><span id="editor" @keydown="commChanged" contenteditable></span><span id="caret"></span>
         </div>
       </div>
     </div>
@@ -21,111 +28,301 @@
 
 <script>
 
-export default {
-  name: 'Terminal',
-  data() {
-    return {
-      command: "",
-      lines: 0,
-      log: [""],
+  export default {
+    name: 'Terminal',
+    data() {
+      return {
+        lines: 0,
 
-      results: [],
+        log: [""],
+        history: [],
+        results: [],
 
-      caretOn: false,
+        form: {
+          active: false,
+          inputIndex: 0,
+          promptText: "Enter your name:",
+          name: "",
+          email: "",
+          content: "",
+        },
 
-      viewingIndex: 0,
-    }
-  },
-  mounted() {
-    setInterval(() => {
-      if(document.getElementById("caret") != null) {
-        if(document.getElementById("caret").innerText === "▋") {
-          document.getElementById("caret").innerText = "";
-        } else {
-          document.getElementById("caret").innerText = "▋";
-        }
+        hangman: {
+          active: false,
+          guessed: [],
+          word: "",
+          wrong: [],
+        },
+
+        readyForNextLine: true,
+
+        viewingIndex: 0,
       }
-    }, 500);
-
-    this.focusInput();
-  },
-  methods: {
-    commChanged(e) {
-      console.log(e);
-      try {
-        if(e.which === 13) {
-          e.preventDefault();
-
-          this.log[this.lines] = e.target.innerText;
-
-          this.lines++;
-          this.viewingIndex = this.lines;
-
-          this.runCommand(this.lines - 1, this.log[this.lines - 1]);
-
-          this.log.push("");
-
-          setTimeout(() => {
-            this.focusInput();
-          }, 75);
-        } else if(e.which === 38 && this.viewingIndex > 0) {
-          e.target.innerText = this.log[--this.viewingIndex];
-        } else if(e.which === 40) {
-          if(this.viewingIndex < this.lines) {
-            e.target.innerText = this.log[++this.viewingIndex];
+    },
+    mounted() {
+      setInterval(() => {
+        if (document.getElementById("caret") != null) {
+          if (document.getElementById("caret").innerText === "▋") {
+            document.getElementById("caret").innerText = "";
+          } else {
+            document.getElementById("caret").innerText = "▋";
           }
-        } else if(e.which === 37 || e.which === 39) {
-          e.preventDefault();
         }
-      } catch(e) {
+      }, 550);
+
+      setInterval(() => {
+        this.focusInput();
+      }, 50);
+    },
+    methods: {
+      commChanged(e) {
         console.log(e);
-      }
-    },
-    focusInput() {
-      if(document.getElementById("editor")) {
-        document.getElementById("editor").focus();
-      }
-    },
-    runCommand(index, command) {
-      let parsedArgs = command.toLowerCase().split(" ");
-      let commOne = parsedArgs[0];
+        try {
+          if (e.which === 13) {
+            e.preventDefault();
 
-      switch(commOne) {
-        case "hello":
-          this.results[index] = "Hello there!";
-          break;
-        case "clear":
-          break;
-        case "github":
-        case "linkedin":
-          let url = this.$store.state["urls"][commOne];
-          this.results[index] = "";
-          window.open(url, "_blank");
-          break;
-        case "help":
-          let helpData = this.$store.state[commOne];
-          this.results[index] = "";
-
-          for(const key in helpData) {
-            this.results[index] += `${key}</br>`;
-            for(const comm in helpData[key]) {
-              this.results[index] += `&emsp;&emsp;${comm}&emsp;&emsp;${ helpData[key][comm] }</br>`
+            if (this.form.active) {
+              this.handleFormEnter(e.target.innerText);
+              e.target.innerText = "";
+              return;
+            } else if(this.hangman.active) {
+              this.handleHangman(e.target.innerText);
+              e.target.innerText = "";
+              return;
             }
-          }
 
-          break;
-        default:
-          this.results[index] = `${commOne} is not a recognised command.<br /> Run 'help' to see the available commands.`
+            this.log[this.lines] = e.target.innerText;
+            if(this.history[this.history.length - 1] !== e.target.innerText) {
+              this.history.push(e.target.innerText);
+            }
+
+            this.runCommand(this.lines, this.log[this.lines]);
+
+            if (this.readyForNextLine) {
+              this.beginNextLine();
+            }
+          } else if (e.which === 38 && this.viewingIndex > 0) {
+            e.target.innerText = this.history[--this.viewingIndex];
+          } else if (e.which === 40) {
+            if (this.viewingIndex < this.history.length - 1) {
+              e.target.innerText = this.history[++this.viewingIndex];
+            } else if(this.viewingIndex === this.history.length - 1) {
+              e.target.innerText = "";
+            }
+          } else if (e.which === 37 || e.which === 39) {
+            e.preventDefault();
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      },
+      focusInput() {
+        if (document.getElementById("editor")) {
+          document.getElementById("editor").focus();
+        }
+      },
+      runCommand(index, command) {
+        let parsedArgs = command.toLowerCase().split(" ");
+        let commOne = parsedArgs[0];
+
+        switch (commOne) {
+          case "hello":
+            this.results[index] = "Hello there!";
+            break;
+          case "clear":
+            this.lines = -1;
+            this.viewingIndex = -1;
+            this.log = [];
+            this.results = [];
+
+            if (index === 0) {
+              document.getElementById("editor").innerText = "";
+            }
+            break;
+          case "light-mode":
+            this.results[index] = "I'm sorry Dave, I'm afraid I can't do that.<br />";
+            break;
+          case "list":
+            this.showResume(index);
+            break;
+          case "about":
+            let ascii = this.$store.state["ascii"];
+            this.results[index] = ascii;
+            this.results[index] += "<div class='textBlock'>";
+            this.results[index] += this.$store.state["about"];
+            this.results[index] += "</div>";
+            break;
+          case "now":
+            this.results[index] += "<br />* Rebuilding my A Level coursework as a progressive web app using Vue.JS and Django.";
+            this.results[index] += "<br />* A music app which generates music through an RNN";
+
+            let now = this.$store.state['now'];
+
+            this.results[index] = "* " + now[0];
+            for(let i = 1; i < now.length; i++) {
+              this.results[index] += "<br />* " + now[i];
+            }
+
+            break;
+          case "info":
+            this.results[index] = `
+            <div class="textBlock">
+              portfolio site version 1.0<br/>
+              using software:<br/>
+              &nbsp; - nuxt (build ^2.14.0)<br />
+              developed with:<br />
+              &nbsp; - my slightly decrepid 2015 15" MacBook Pro<br />
+              &nbsp; - sheer determination, plenty of bugs, and many cups of tea
+            </div>
+          `;
+            break;
+          case "email":
+            this.results[index] = "Your wish is my command... you can get in touch me with on:<br />";
+            this.results[index] += `<a href='mailto://${this.$store.state['email']}'>${this.$store.state['email']}</a>`;
+            break;
+          case "hangman":
+            this.readyForNextLine = false;
+            this.hangman.active = true;
+            this.handleHangman("");
+            return;
+          case "github":
+          case "linkedin":
+            let url = this.$store.state["urls"][commOne];
+            this.results[index] = "";
+            window.open(url, "_blank");
+            break;
+          case "form":
+            this.results[index] = "";
+            this.form.active = true;
+            this.readyForNextLine = false;
+            return;
+          case "help":
+            this.showHelp(commOne, index);
+            break;
+          default:
+            this.results[index] = `${commOne} is not a recognised command.<br /> Run 'help' to see the available commands.`
+        }
+
+        this.readyForNextLine = true;
+      },
+      showHelp(commOne, index) {
+        let helpData = this.$store.state[commOne];
+        this.results[index] = "";
+
+        for (const key in helpData) {
+          this.results[index] += `${key}</br>`;
+          for (const comm in helpData[key]) {
+            this.results[index] += `&emsp;&emsp;${comm}&emsp;&emsp;${helpData[key][comm]}</br>`
+          }
+        }
+      },
+      handleFormEnter(text) {
+        this.results[this.lines] += `${this.form.promptText} ${text}<br />`;
+        this.form.promptText = ["Enter your email:", "Your message:", "Enter your name:"][this.form.inputIndex];
+
+        if(this.form.inputIndex === 0) {
+          this.form.name = text;
+        } else if(this.form.inputIndex === 1) {
+          this.form.email = text;
+        } else if(this.form.inputIndex === 2) {
+          this.form.content = text;
+          this.form.active = false;
+          this.form.inputIndex = 0;
+
+          this.results[this.lines] += "<br />Thank you so much for your message, I'll get back to you as soon as possible!<br />"
+
+          this.beginNextLine();
+          return;
+        }
+
+        this.form.inputIndex++;
+      },
+      beginNextLine() {
+        this.lines++;
+        this.viewingIndex = this.history.length;
+
+        this.readyForNextLine = true;
+
+        this.log.push("");
+
+        setTimeout(() => {
+          this.focusInput();
+        }, 75);
+      },
+      showResume(index) {
+        this.results[index] = "";
+
+        const workplaces = this.$store.state['workplaceTitles'];
+        const titles = this.$store.state['jobTitles'];
+        const descriptions = this.$store.state['jobDescriptions'];
+
+        for(let i = workplaces.length - 1; i >= 0; i--) {
+          this.results[index] += workplaces[i];
+          this.results[index] += `<strong>${titles[i]}</strong>`;
+          this.results[index] += `<br /><div class="wideTextBlock">${descriptions[i]}</div>`;
+          this.results[index] += "<span>------------------------------------------------</span><br />"
+        }
+      },
+      handleHangman(letter) {
+        let buffer = this.results[this.lines];
+        this.lines++;
+
+        if(this.hangman.guessed.length === 0 && this.hangman.wrong.length === 0 && this.hangman.word === "") {
+          this.hangman.word = this.$store.state.hangmanWords[Math.floor(Math.random() * 200)];
+          buffer = "";
+          letter = "";
+        } else if(letter.length > 1) {
+          buffer += "Only enter one letter please!<br />";
+        } else if(this.hangman.word.includes(letter)) {
+            if(!this.hangman.guessed.includes(letter)) {
+              this.hangman.guessed.push(letter);
+            }
+        } else if(!this.hangman.word.includes(letter) && !this.hangman.wrong.includes(letter)) {
+          buffer += letter + " is not a part of the word!<br />";
+          this.hangman.wrong.push(letter);
+
+          buffer += `<pre>${ this.$store.state.hangmanAscii[this.hangman.wrong.length - 1] }</pre><br />`;
+
+          let max = this.$store.state.hangmanAscii.length;
+          if(this.hangman.wrong.length >= max) {
+            buffer += `Oh no, you lost... the word was '${ this.hangman.word }'<br />`;
+            this.hangman.active = false;
+            this.results[--this.lines] = buffer;
+            this.beginNextLine();
+            return;
+          }
+        }
+
+        buffer += `<br />Wrong Guesses: ${this.hangman.wrong}<br />`;
+        buffer += `Word: `;
+        let won = true;
+        for(let letter of this.hangman.word.split("")) {
+          if(this.hangman.guessed.includes(letter)) {
+            buffer += letter + " ";
+          } else {
+            buffer += "_ ";
+            won = false;
+          }
+        }
+        buffer += "<br /><br />";
+
+        if(won) {
+          buffer += `Congratulations! You correctly guessed the word.<br />`;
+          this.readyForNextLine = true;
+          this.hangman.active = false;
+        }
+
+        this.lines--;
+        this.results[this.lines] = buffer;
       }
-    }
-  },
-}
+    },
+  }
 </script>
 
 <style>
   .terminal {
     width: 100vw;
-    height: 100vh;
+    height: calc(100vh - 20px);
     margin: 0;
     padding: 20px;
     overflow-y: scroll;
@@ -137,8 +334,8 @@ export default {
     background-color: #222222;
     color: #F5F5F5;
 
-    -ms-overflow-style: none;  /* IE and Edge */
-    scrollbar-width: none;  /* Firefox */
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
   }
 
   .terminal::-webkit-scrollbar {
@@ -163,5 +360,28 @@ export default {
 
   .is-red {
     color: indianred;
+  }
+
+  .textBlock {
+    max-width: 75ch;
+  }
+
+  .wideTextBlock {
+    max-width: 100ch;
+  }
+
+  pre.workplaceTitle {
+    font-size: 8px;
+    padding-left: 0;
+    background: transparent;
+  }
+
+  a {
+    color: mediumseagreen;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
   }
 </style>
--- store/index.js
+++ store/index.js
@@ -1,16 +1,194 @@
 export const state = () => ({
   help: {
     "seeing my work": {
-      "ls": "lists all my previous and current projects",
+      "list": "lists all my previous and current experience",
+      "now": "what am i working on at the moment?",
+      "projects": "my favourite personal and academic projects",
+      "about": "a little bit about me",
+      "blog": "a student's perspective on this and that",
+    },
+    "my other pages": {
       "github": "opens my GitHub page",
-      "linkedin": "opens my LinkedIn page"
-    }
+      "linkedin": "opens my LinkedIn page",
+    },
+    "getting in touch": {
+      "email": "shows you my email address",
+      "form": "lets you fill out a contact form",
+    },
+    "fun things": {
+      "hangman": "fancy a game of hangman?",
+      "console-wars": "alien ships on your tail, shake 'em off",
+    },
+    "nerdy things": {
+      "help -v": "run help in nerd mode",
+      "info": "how did this website come about",
+      "light-mode": "revert the console colours",
+    },
   },
+  email: "hello@joerourke.co.uk",
   hello: {
-    "there": "General Kenobi."
+    "there": "General Kenobi.",
   },
+  work: {},
+  about: `Hi, I am a Full Stack Engineer particularly interested in 
+          machine learning. Currently, I am studying AI at Royal Holloway, University of London.
+          I am also interested in politics and the environment, and how technology can be used to
+          fight in these causes (check out my blog for more on this!). <br />
+          I'm always on the lookout for the latest opportunities and technologies, so please do get in touch!<br />`,
   urls: {
     "github": "https://github.com/joerourke123",
     "linkedin": "https://linkedin.com/in/joe-rourke",
+  },
+
+  now: [
+    "I'm always doing work on this site, and my blog where I write about technology, politics, and the environment!",
+    "Rebuilding my A Level coursework as a progressive web app using Vue.JS and Django.",
+    "A music app which generates music through an RNN"
+  ],
+
+  ascii: `<pre style="width: 50%; background: transparent;">
+     /\\\\\\\\\\\\\\\\\\\\
+     || _   _ ||
+    (@ (o) (o) @)
+      |   ||  |
+      |   __/ |
+       \\______/
+     ____|  |____
+    /    \\__/     \\
+   /               \\
+  /__/|   joe   |\\__\\
+ / /  |         | \\  \\
+ </pre>
+ `,
+
+  workplaceTitles: [
+    `<pre class="workplaceTitle">
+ ___                       _     _  _         _   _                                       ___   ___     ___                              _                          _   
+| _ \\  ___   _  _   __ _  | |   | || |  ___  | | | |  ___  __ __ __  __ _   _  _         / __| / __|   |   \\   ___   _ __   __ _   _ _  | |_   _ __    ___   _ _   | |_ 
+|   / / _ \\ | || | / _\` | | |   | __ | / _ \\ | | | | / _ \\ \\ V  V / / _\` | | || |  _    | (__  \\__ \\   | |) | / -_) | '_ \\ / _\` | | '_| |  _| | '  \\  / -_) | ' \\  |  _|
+|_|_\\ \\___/  \\_, | \\__,_| |_|   |_||_| \\___/ |_| |_| \\___/  \\_/\\_/  \\__,_|  \\_, | ( )    \\___| |___/   |___/  \\___| | .__/ \\__,_| |_|    \\__| |_|_|_| \\___| |_||_|  \\__|
+             |__/                                                           |__/  |/                                |_|                                                 
+</pre>`,
+    `<pre class="workplaceTitle">
+                ___  
+  ___   _ _    / _ \\ 
+ / _ \\ | ' \\  | (_) |
+ \\___/ |_||_|  \\__\\_\\
+                             
+</pre>`,
+    `<pre class="workplaceTitle">
+ ___                     _                      _          _ 
+| __|  _ _   ___   ___  | |  __ _   _ _    __  (_)  _ _   ( )
+| _|  | '_| / -_) / -_) | | / _\` | | ' \\  / _| | | | ' \\  |/ 
+|_|   |_|   \\___| \\___| |_| \\__,_| |_||_| \\__| |_| |_||_|                                                                  
+</pre>
+    `,
+    `<pre class="workplaceTitle">
+ ___   _                                      __  __
+/ __| | |_    __ _   _ _   _ __   ___   __ _  \\ \\/ /
+\\__ \\ | ' \\  / _\` | | '_| | '_ \\ |___| / _\` |  &gt;  &lt; 
+|___/ |_||_| \\__,_| |_|   | .__/       \\__,_| /_/\\_\\
+                          |_|                       
+</pre>
+    `,
+    `<pre class="workplaceTitle">
+ ___                              _            ___ 
+| __|  ___   _ _   _ __    _  _  | |  __ _    | __|
+| _|  / _ \\ | '_| | '  \\  | || | | | / _\` |   | _| 
+|_|   \\___/ |_|   |_|_|_|  \\_,_| |_| \\__,_|   |___|
+</pre>
+    `,
+  ],
+  jobTitles: [
+    "Undergraduate AI Researcher (Jul - Sep 2020)",
+    "Full Stack Engineer (May - Oct 2020)",
+    "Freelance Developer (Jan 2020 - )",
+    "Software Support Engineer (July 2018)",
+    "Web Developer / IT Support (June - July 2017)"
+  ],
+  jobDescriptions: [
+    "I worked alongisde academics and postgraduate students in the Computer Science department in order to adapt previous research into automated negotiation to create a new bileratal negotaition strategy. The strategy can cope with uncertain preferences and implemented in Java for use with the GENIUS negotiation simulation environment. It required use of Microsoft's Z3 solver program in order to provide preferences in uncertain environments. A full research paper based on my findings is in the works.",
+    "I developed a web and mobile application alongside a colleague for a new company under Callagenix. The application provided companies, primarily clinical practices, with a queue and appointment management system for easing pressure on clients and staff throughout the COVID-19 pandemic. I had responsibility for the database and web server set up, using Python's Django library and Postgres for the backend. The frontend design was my main task though, using Vue.JS to produce a elegant and powerful web application. A mobile version of the application was also under works, using the Flutter framework for cross platform development. Unfortunately, the project was abandoned prior to full release.",
+    "As a student, I work also as a freelance developer - mainly in web and software commissions - to gain some extra funds. In these projects, ranging from local business landing sites to embedded Raspberry Pi-based systems written with C++, I constantly elicit details from the client and provide them with long term maintenance of their projects after delivery. These experiences have given me a lot of confidence in my development abilities and chance to maintain contact and meet clients.",
+    "In this work experience placement, I got to experience a real software company preparing for a new release. While most of my work lied with the support team helping clients diagnose and solve problems with the company's software, I was able to observe and discuss the process of building and releasing a real software package with their developers. This role taught me about communication and conducting myself in a workplace environment, which has helped me in future professional and academic endeavors.",
+    "While doing work experience at Formula E in London, I helped the web developer and tech support teams in their daily duties, diagnosing and fixing staff computers and equipment, and helping diagnose bugs in their new website's test release. I learnt about jQuery for speeding up frontend development and how web developers work together using tools like Slack and Git."
+  ],
+
+  hangmanWords: ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom", "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", "blizzard", "boggle", "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "euouae", "exodus", "faking", "fishhook", "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled", "frizzled", "fuchsia", "funny", "gabby", "galaxy", "galvanize", "gazebo", "giaour", "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip", "grogginess", "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "ivy", "jackpot", "jaundice", "jawbreaker", "jaywalk", "jazziest", "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "kiosk", "kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury", "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm", "pixel", "pizazz", "pneumonia", "polka", "pshaw", "psyche", "puppy", "puzzling", "quartz", "queue", "quips", "quixotic", "quiz", "quizzes", "quorum", "razzmatazz", "rhubarb", "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "spritz", "squawk", "staff", "strength", "strengths", "stretch", "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress", "transplant", "triphthong", "twelfth", "twelfths", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka", "voodoo", "vortex", "voyeurism", "walkway", "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard", "woozy", "wristwatch", "wyvern", "xylophone", "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", "zodiac", "zombie"],
+  hangmanAscii: [`
+  +---+
+  |   |
+  |
+  |
+  |
+  |
+  =========`, `
+  +---+
+  |   |
+  O   |
+  |
+  |
+  |
+  =========`, `
+  +---+
+  |   |
+  O   |
+  |   |
+  |
+  |
+  =========`, `
+  +---+
+  |   |
+  O   |
+  /|   |
+  |
+  |
+  =========`, `
+  +---+
+  |   |
+  O   |
+  /|\  |
+  |
+  |
+  =========`, `
+  +---+
+  |   |
+  O   |
+  /|\  |
+    /    |
+    |
+    =========`, `
+    +---+
+    |   |
+    O   |
+    /|\  |
+    / \  |
+    |
+    =========`]
+});
+
+export const actions = () => ({
+  sendEmail(store, { name, email, content }) {
+    // const sgMail = require('@sendgrid/mail');
+    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    // const msg = {
+    //   to: email, // Change to your recipient
+    //   from: "portfolio@joerourke.co.uk", // Change to your verified sender
+    //   subject: "New Message from Portfolio!",
+    //   text: `
+    //   ${content}
+    //
+    //   from,
+    //   ${name}
+    //   `,
+    // };
+    // sgMail
+    //   .send(msg)
+    //   .then(() => {
+    //     console.log("Email Sent!");
+    //   })
+    //   .catch((error) => {
+    //     console.error(error)
+    //   })
   }
 });
