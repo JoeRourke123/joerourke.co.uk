f8a545495f8bc35f166d81e8541a3b2fd43e63b9
JoeRourke123 <joseph.rourke.2019@live.rhul.ac.uk>
2020-10-16 13:19:34 +0100

    Adding in a blog section which uses Notion pages in order to display blog content

---
+++ pages/blog/_id.vue
@@ -0,0 +1,28 @@
+<template>
+  <iframe @load="changedURL" :src="computedURL" frameborder="0"></iframe>
+</template>
+
+<script>
+  export default {
+    name: "_id.vue",
+    data() {
+      return {
+        id: this.$route.params.id,
+      }
+    },
+    computed: {
+      computedURL() {
+        return "https://notion.so/joerourke/" + id;
+      }
+    },
+    methods: {
+      changedURL(e) {
+        console.log(e);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="loading-overlay"></div>
+    <iframe @load="changedURL" src="http://localhost:3000" frameborder="0">
+
+    </iframe>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "index.vue",
+      methods: {
+        changedURL(e) {
+          if(e.target.src === this.comptuedURL) {
+            
+          }
+        }
+      },
+      computed: {
+        computedURL() {
+          return "https://notion.so/joerourke/joes-blog";
+        }
+      }
+    }
+</script>
+
+<style>
+  .loading-overlay {
+
+  }
+
+  iframe {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
